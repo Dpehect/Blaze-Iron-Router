eed568fd8e238965b51f34697c612d3d1d048b69
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2024-03-14 17:01:35 +0300

    First Commit

---
+++ routes.js
@@ -0,0 +1,17 @@
+import { Router } from 'meteor/iron:router';
+
+Router.configure({
+  layoutTemplate: 'layout' 
+});
+
+Router.route('/', function () {
+  this.render('home');
+});
+
+Router.route('/about', function () {
+  this.render('about');
+});
+
+Router.route('/contact', function () {
+    this.render('contact');
+  });
